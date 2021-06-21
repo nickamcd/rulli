250be0a4342b0cf9f7fc48e75c2e0262bcc3350a
Nick McDaniel <nickamcd@gmail.com>
2021-06-21 12:55:24 -0700

    Added about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,46 @@
 const About = () => {
   return (
     <div className="about">
-      <h1>About</h1>
+      <h1>About Emporio Rulli</h1>
+      <p>
+        The Emporio Rulli concept is about creating a 
+        genuine, authentic Italian coffee and pastry 
+        bar experience for our guests in the San 
+        Francisco Bay Area.
+      </p>
+      <p>
+        As a 4th generation native San Franciscan 
+        whose grandparents and great grandparents 
+        emigrated to the San Francisco Bay Area in the 
+        early 20th century, we have, for the past 33 
+        years, been ambassadors of Italian culture and 
+        gastronomy.  Everything you see in our beautiful, 
+        Italian-designed show cases is made 365 days 
+        a year in our pastry and savory kitchen in 
+        Larkspur.  Our selection of artisan handmade 
+        pastries, breads, paninis, macarons, salads 
+        and handcrafted coffees are all unique and 
+        authentic and are only found in our family 
+        run operation.
+      </p>
+      <p>
+        Being the only Italian American inducted into AMPI, 
+        Accademia Maestri Pasticceria Italiana (Italian 
+        Pastry Masters Academy) in Brescia, Italy, keeps us 
+        in the forefront of all the latest pastry, coffee 
+        and food trends as well as centuries’ old traditions 
+        to surprise and delight our customers on a daily 
+        basis.  Our coffee is roasted bi-weekly in our 
+        roasting plant by Chef Rulli himself, having been 
+        trained by Alfred Peet (well-known as the creator 
+        of the artisan coffee movement as well as the 
+        founder of Peets Coffee & Tea). 
+      </p>
+      <p>
+        We are unique in the Bay Area for both roasting coffee 
+        and producing all of our products in house, setting us 
+        apart from all of our competitors.  
+      </p>
     </div>
   )
 }
