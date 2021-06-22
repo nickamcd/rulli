6fb875a7df61bca60e51f480f9c4b9a61170688a
Nick McDaniel <nickamcd@gmail.com>
2021-06-22 09:26:15 -0700

    Added careers page with dummy links

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -2,6 +2,50 @@ const Careers = () => {
   return (
     <div className="careers page">
       <h1>Careers</h1>
+      <p>
+        Become a part of the Emporio Rulli family.  
+        Based in Larkspur, this small business has grown 
+        to 5 locations giving the Bay Area a taste of Italy.  
+        Famed for his panettone, Chef Gary Rulli takes pride 
+        in producing high quality products with the best 
+        ingredients and the best people.
+      </p>
+      <p>
+        Emporio Rulli is always looking for talented, hardworking 
+        and friendly team members.  Whether you’re just diving 
+        into the workforce or your career is changing course your 
+        new experience could be waiting for you at Emporio Rulli, 
+        below are some of the opportunities we currently have 
+        available.
+      </p>
+      <p>
+        All of our positions require a California Food Handlers 
+        Certificate, you must be eligible to work in the United States, 
+        and have the ability to read and understand English.  
+        Please feel free to apply of any of our open positions by 
+        emailing <a href="#">info@rulli.com</a>, or better yet, stop in to see us 
+        in person.
+      </p>
+      <h3>Larkspur</h3>
+      <div>
+        <p>Learn More about these positions:</p>
+        <ul>
+          <li><a href="#">Barista</a></li>
+          <li><a href="#">Pastry Attendant</a></li>
+          <li><a href="#">Swing Shift Maintenance/Janitorial</a></li>
+          <li><a href="#">Full/Part Time Delivery Driver</a></li>
+        </ul>
+      </div>
+      <h3>Union Square San Francisco Il Caffe' & Bancarella</h3>
+      <div>
+        <p>Learn More about these positions:</p>
+        <ul>
+          <li><a href="#">Caffe Attendant</a></li>
+          <li><a href="#">Assistant Store Manager</a></li>
+          <li><a href="#">Shift Leader</a></li>
+        </ul>
+      </div>
+      <h3>Chestnut Street  (Opening Soon)</h3>
     </div>
   )
 }
